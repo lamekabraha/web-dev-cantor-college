74bb9c2253ee3a797d6450ddc458f92c8ce66351
lamekabraha <115846138+lamekabraha@users.noreply.github.com>
2025-12-26 00:36:51 +0000

    first commit

---
+++ CantorCollege/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import Welcome from './Components/Welcome'
+import Header from './Components/Header/Header'
+
+const App = () => {
+
+  return (
+    <>
+    <Header />
+    <Welcome />
+    <h1>Hello World</h1>
+    </>
+  )
+}
+
+export default App
---
+++ CantorCollege/src/Components/Header/Header.jsx
@@ -0,0 +1,10 @@
+import './Header.css';
+
+const Header = () =>{
+    return(
+        <img className='headerImg' src="./logo.png" alt="Cantor College logo"/>
+        
+    )
+}
+
+export default Header;
